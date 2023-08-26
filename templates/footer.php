d7b7e5902469f2e0b65b9ee4ba4381c0fe3156e2
Zihan Abedin <101731382+zihanabedin1999@users.noreply.github.com>
2023-08-26 19:17:33 +0600

    Add files via upload

---
+++ templates/footer.php
@@ -0,0 +1,8 @@
+<div ID="footer">
+	<h2>&copy; www.zihan.com</h2>
+	</div>
+	</div>
+	
+
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+ <head>
+  <title>SOCIAL NETWORK</title>
+  <link rel ="stylesheet" href ="styles/style.css" media ="all"/>
+  </head>
+  
+<body>
+ <div class="container">
+    <div ID="header_wrapper">
+	<div ID="header">
+	<img  src="images/Lion.jpg" style = "float:left;" height="100" width="100" />
+	<form method ="post" action="" ID="form1">
+	<strong>Email:</strong>
+	<input type="email" name="email" placeholder="Email" required="required"/>
+	<strong>Password:</strong>
+	<input type="password" name="pass" placeholder="******" required="required"/>
+	<button name="login">Login</button>
+	</form>
+	
+	</div>
+	</div>
+	
+</html>
